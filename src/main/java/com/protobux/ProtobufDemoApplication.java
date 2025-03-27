47bde90d2e079168292c6a9f936796c2b534baa2
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-03-27 09:44:51 +0530

    initializing project

---
+++ src/main/java/com/protobux/ProtobufDemoApplication.java
@@ -0,0 +1,13 @@
+package com.protobux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protobux/ProtobufDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protobux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
